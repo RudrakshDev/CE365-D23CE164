da8122516e27b9f5652f7b68299cf231d117e84a
RudrakshDev <wis40311@gmail.com>
2025-02-13 13:11:16 +0530

    Create Practical-1-D23CE164.c
    Add Practical-1-D23CE164.c

---
+++ Practical-1-D23CE164.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int isValidString(const char *str) {
+    int length = strlen(str);
+    int i = 0;
+    
+    if (length < 2 || str[length - 1] != 'b' || str[length - 2] != 'b') {
+        return 0; // Invalid string
+    }
+    
+    while (i < length - 2) {
+        if (str[i] != 'a') {
+            return 0; // Invalid string if not 'a' before 'bb'
+        }
+        i++;
+    }
+    
+    return 1; // Valid string
+}
+
+int main() {
+    char input[100];
+    
+    printf("Enter a string: ");
+    fgets(input, sizeof(input), stdin);
+    
+    input[strcspn(input, "\n")] = '\0';
+    
+    if (isValidString(input)) {
+        printf("Valid String\n");
+    } else {
+        printf("Invalid String\n");
+    }
+    
+    return 0;
+}
